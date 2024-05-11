c5ed8993f42f0ca2ea4472f2fbb706aed08e6e3d
kimjaehui02 <momg4287@naver.com>
2024-05-11 23:44:35 +0900

    불필요한 주석 정리

--- scrapping.lua
+++ scrapping.lua
@@ -1,90 +1,15 @@
 
--- data:extend
--- {  
-  -- {
-  --   type = "recipe",
-  --   name = "electronic-circuit-scrapping",
-  --   category = "scrapping",
-  --   energy_required = 2,
-  --   enabled = true,
-  --   ingredients = {{"electronic-circuit", 1}},
-  --   icon = "__base__/graphics/icons/electronic-circuit.png",
-  --   icon_size = 64, icon_mipmaps = 4,
-  --   subgroup = "raw-material",
-  --   order = "k[uranium-processing]", -- k ordering so it shows up after explosives which is j ordering
-  --   results =
-  --   {
-  --     {
-  --       name = "iron-plate",
-  --       probability = 0.25,
-  --       amount = 1
-  --     },
-  --     {
-  --       name = "copper-cable",
-  --       probability = 0.75,
-  --       amount = 1
-  --     }
-  --   }
-  -- },
---   {
---     type = "recipe",
---     name = "transport-belt-scrapping",
---     category = "scrapping",
---     energy_required = 2,
---     enabled = true,
---     ingredients = {{"transport-belt", 1}},
---     icon = "__base__/graphics/icons/transport-belt.png",
---     icon_size = 64, icon_mipmaps = 4,
---     subgroup = "raw-material",
---     order = "k[uranium-processing]", -- k ordering so it shows up after explosives which is j ordering
---     results =
---     {
---       {
---         name = "iron-plate",
---         probability = 0.25,
---         amount = 1
---       },
---       {
---         name = "iron-gear-wheel",
---         probability = 0.25,
---         amount = 1
---       }
---     }
---   },
--- }
-
 local new_recipes = {}
 local new_recipe
 
 for _, recipe in pairs(data.raw["recipe"]) do
 
-
---   if data.raw["item"][recipe.name or ""] or data.raw["item"][recipe.result or ""] or (recipe.results and recipe.results[1] and data.raw["item"][recipe.results[1].name or ""]) then
---     -- recipe.name, recipe.result, recipe.results[1]이 모두 nil일 때 스킵
---     goto continue
--- end
 if (data.raw["item"][recipe.name or recipe.result or recipe.results[1].name] ==nil) then
   goto continue
 end
 
 
--- if (recipe.results and recipe.results[1] and data.raw["item"][recipe.results[1].name or ""]) then
---   goto continue
--- end
 
---   local mainString = recipe.name
---   local subString = "barrel"
---   local subString2 = "empty"
---   local subString3 = "fill"
-  
---   if string.find(mainString, subString) then
---     if string.find(mainString, subString2) then
---       goto continue
---   end
---   if string.find(mainString, subString3) then
---     goto continue
--- end
---   end
 
 
   if not recipe.category or recipe.category == "crafting" or recipe.category == "advanced-crafting"  or  recipe.category == "crafting-with-fluid"  then
@@ -153,13 +78,6 @@ end
 
     end
     
-
-      -- table.insert(new_recipe.results, 
-      -- {
-      --   name = recipe.normal.ingredients[1][1] or recipe.normal.ingredients[1].name,
-      --   probability = 0.2,
-      --   amount = 1
-      -- })
   else
       -- 일반적인 레시피의 처리
       new_recipe = 
@@ -217,35 +135,11 @@ end
           amount = amountinput
       })
     end
-        -- table.insert(new_recipe.results, 
-        -- {
-        --     name = recipe.ingredients[input][1] or recipe.ingredients[input].name,
-        --     probability = probabilityinput,
-        --     amount = amountinput
-        -- })
+
     end
       -- 처리할 코드 작성
   end
 
-  -- 새로운 레시피 데이터 생성
-
-
-  
-  -- {
-  --     type = "recipe",
-  --     name = new_recipe_name,
-  --     enabled = true,
-  --     ingredients = recipe.result, -- 결과물이어야 함
-  --     result = recipe.ingredients, -- 재료가어야 함
-  --     energy_required = 2
-  -- }
-
-  -- table.insert(new_recipe.results, 
-  -- {
-  --   name = recipe.name,
-  --   probability = 0.1,
-  --   amount = 1
-  -- })
   table.insert(new_recipes, new_recipe)
 end
 ::continue::
